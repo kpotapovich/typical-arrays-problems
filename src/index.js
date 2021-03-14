27ead3bf645c417b01af377ba57ae8250466e22d
kpotapovich <kris.potapovich@gmail.com>
2021-03-14 23:52:54 +0300

    init: minimal value, maximal value, and an average value of a given array.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array) {
+        if (array.length > 0) {
+            return Math.min(...array);
+        }
+    }
+    return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array) {
+        if (array.length > 0) {
+            return Math.max(...array);
+        }
+    }
+    return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array) {
+        if (array.length > 0) {
+            let sum = 0;
+            for (let i = 0; i < array.length; i++) {
+                sum = sum + array[i];
+            }
+            return sum / array.length;
+        }
+    }
+    return 0;
+
 }
